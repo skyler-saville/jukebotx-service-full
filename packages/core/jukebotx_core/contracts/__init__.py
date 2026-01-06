660de51688d1c5389ac80dfd2a650b8428f08ef3
Skyler Saville <35354111+skyler-saville@users.noreply.github.com>
2026-01-06 00:37:54 -0700

    Add activity DTOs and API endpoints

---
+++ packages/core/jukebotx_core/contracts/__init__.py
@@ -0,0 +1,15 @@
+from jukebotx_core.contracts.activity import (
+    EventEnvelope,
+    NowPlayingDTO,
+    QueueItemDTO,
+    ReactionCountDTO,
+    SessionStateDTO,
+)
+
+__all__ = [
+    "EventEnvelope",
+    "NowPlayingDTO",
+    "QueueItemDTO",
+    "ReactionCountDTO",
+    "SessionStateDTO",
+]
